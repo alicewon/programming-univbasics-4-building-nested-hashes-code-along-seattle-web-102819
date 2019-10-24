6f83b3d687eae78741fa328c90676d1ad7cadb88
Alice Won <alicewon4@gmail.com>
2019-10-24 21:22:40 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -2,7 +2,7 @@ def first_challenge
    epic_tragedy = {
      :montague => {
        :patriarch => {name: "Lord Montegue", age: "53"}
-       :matriarch =>{name: "Lady Montegue", age: "54"}
+       :matriarch => {name: "Lady Montegue", age: "54"}
        :hero => {name: "Romeo", age: "15", status: "alive"}
        :hero_friends => [
          {name: "Benvolio", age: "17", attitude: "worried"},
@@ -11,7 +11,7 @@ def first_challenge
      },
      :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"}
-      :matriarch =>{name: "Lady Capulet", age: "51"}
+      :matriarch => {name: "Lady Capulet", age: "51"}
       :heroine => {name: "Juliet", age: "15", status: "alive"}
       :heroine_friends => [
         {name: "Steven", age: "30", attitude: "confused"},
